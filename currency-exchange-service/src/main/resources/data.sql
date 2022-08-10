590ffb855beb7bcd10e00e2810bdaa9ad84e112f
Dinesh <dinesh.dl95@gmail.com>
2022-08-10 12:57:54 +0530

    Initial Commit

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into currency_xchange(id,currency_from,currency_to,conversion_multiple,environment) values (1001,'USD','INR',65,'');
+
+insert into currency_xchange(id,currency_from,currency_to,conversion_multiple,environment) values (1002,'AUD','INR',70,'');
+
+insert into currency_xchange(id,currency_from,currency_to,conversion_multiple,environment) values (1003,'EUR','INR',102,'');
